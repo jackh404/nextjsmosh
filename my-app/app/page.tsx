69a116a93bd4afcc83a53daf7905031e77792a93
Jack Henza <jackhenza@gmail.com>
2023-12-28 19:24:52 +0000

    learn next

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,7 +1,13 @@
 import Image from 'next/image'
+import Link from 'next/link'
+import ProductCard from './components/ProductCard'
 
 export default function Home() {
   return (
-    <main>Hello World</main>
+    <>
+      <main>Hello World</main>
+      <Link href="/users">Users</Link>
+      <ProductCard />
+    </>
   )
 }
